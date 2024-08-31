b89f24b7bf725b37ab34fc91de89c51c532c605b
Nishant Wailkar <nishantw631@gmail.com>
2024-08-31 16:10:50 +0530

    added edit route

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 const Listing = require("./models/listing");
 const path = require("path");
+const methodOverride = require("method-override");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/VistaStay";
 
@@ -26,6 +27,7 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 app.get("/", (req, res) => {
   res.send("In the Home Route Now");
@@ -50,18 +52,33 @@ app.get("/listings/:id", async (req, res) => {
   res.render("listings/show.ejs", { listing });
 });
 
+// Posting , route
 app.post("/listings", async (req, res) => {
-  let { title, description, image, price, location, country } = req.body;
-  let newListing = new Listing({
-    title: title,
-    description: description,
-    image: image,
-    price: price,
-    location: location,
-    country: country,
-  });
+  // let { title, description, image, price, location, country } = req.body;
+  // let newListing = new Listing({
+  //   title: title,
+  //   description: description,
+  //   image: image,
+  //   price: price,
+  //   location: location,
+  //   country: country,
+  // });
+  let newListing = new Listing(req.body.listing);
 
   await newListing.save();
+  res.redirect("/listings");
+});
+
+// Edit Route
+app.get("/listings/:id/edit", async (req, res) => {
+  let { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listings/edit.ejs", { listing });
+});
+
+app.put("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  await Listing.findByIdAndUpdate(id, { ...req.body.listing });
   res.redirect("/listings");
 });
 
